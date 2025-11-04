ae4a47d1ee4fcb40a6629c9600623b218c7ebedb
khalidzahra <khalidzahra923@gmail.com>
2025-11-04 08:07:10 -0600

    Implement VGA buffer interface

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,16 @@
 
 use core::panic::PanicInfo;
 
-static MOTEOS: &[u8] = b"MoteOS";
+mod vga_buffer;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in MOTEOS.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("Hello World{}", "!");
     loop {} 
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
